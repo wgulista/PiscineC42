efdfe0e20558963d5377016f87006e458a1bf1e6
Wasim GULISTAN <wgulista@e2r1p8.42.fr>
2014-09-17 14:41:06 +0200

    Rotone

---
+++ rotone/rotone.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotone.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wgulista <wgulista@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/09/17 14:20:18 by wgulista          #+#    #+#             */
+/*   Updated: 2014/09/17 14:35:18 by wgulista         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	rotone(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a' && str[i] < 'z') || (str[i] >= 'A' && str[i] < 'Z'))
+			ft_putchar(str[i] + 1);
+		else if (str[i] == 'z' || str[i] == 'Z')
+			ft_putchar(str[i] - 25);
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+	str[i] = '\0';
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		rotone(argv[1]);
+	}
+	return (0);
+}
