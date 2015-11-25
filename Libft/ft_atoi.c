e5eaa3eb727c5916066cf1f24665169f83bed65b
Gulistan Wasim <gitWasimGulistan@gmail.com>
2015-11-25 23:35:05 +0100

    Ajout de la fonction ft_atoi

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+int		ft_atoi(const char *str)
+{
+	int	i;
+	int num;
+	int result;
+
+	i = 0;
+	result = str[i] - '0';
+	while (str[i] != '\0')
+	{
+		if(str[i] >= 48 && str[i] <= 57)
+		{
+			result *= 10;
+			num = str[i] - '0';
+			result += num;
+		}
+		else
+			result = 0;
+		i++;
+	}
+	return (result);
+}                       
