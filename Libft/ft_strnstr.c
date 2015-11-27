6f6ed99e7c65e233d21c18c0446007e2f581c23f
Gulistan Wasim <gitWasimGulistan@gmail.com>
2015-11-27 14:16:33 +0100

    Ajout des fonctions ft_strstr et ft_strnstr

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -1,32 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_str2str.c                                        :+:      :+:    :+:  */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wgulista <wgulista@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/26 14:47:56 by wgulista          #+#    #+#             */
-/*   Updated: 2015/11/26 18:38:57 by wgulista         ###   ########.fr       */
+/*   Created: 2015/11/27 14:02:56 by wgulista          #+#    #+#             */
+/*   Updated: 2015/11/27 14:02:57 by wgulista         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strstr(const char *s1, const char *s2)
+char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
-	int	i;
-	int	j;
-	int	k;
+	size_t		i;
+	size_t		j;
+	int		c;
 
 	i = 0;
-	if (s2[0] == '\0')
-		return ((char *)s1);
-	while (s1[i] != '\0')
+	c = 0;
+	if(s2[0] == '\0')
+		return ((char *) s1);
+	while (i < n && s1[i] != '\0')
 	{
 		j = i;
 		c = 0;
-		while (s1[j] == s2[c])
-			return ((char *)&s1[i]);
+		while (s1[j] == s2[c] && j < n)
+		{
+			j++;
+			c++;
+			if (s2[c] == '\0')
+				return ((char *)&s1[i]);
+		}
 		i++;
 	}
 	return (NULL);
