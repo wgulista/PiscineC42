33cfce2e7448328054e88e66cd6e80f97788466a
Wasim Gulistan <underground_7@hotmail.fr>
2016-02-12 19:10:28 +0100

    tst

--- Fillit/srcs/error.c
+++
@@ -1,7 +0,0 @@
-#include "../includes/fillit.h"
-
-void  ft_error()
-{
-  ft_putendl("error");
-  exit(EXIT_FAILURE);
-}
--- Fillit/srcs/fillit.c
+++
@@ -1,6 +0,0 @@
-#include "../includes/fillit.h"
-
-void  fillit(char **av)
-{
-  (void)av;
-}
