b87383acec7e3e58e671f5a3e3f08d8a72b822d9
Wasim GULISTAN <wgulista@e1r13p13.42.fr>
2016-03-05 12:29:54 +0100

    norminette

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,70 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wgulista <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/05 12:25:22 by wgulista          #+#    #+#             */
+/*   Updated: 2016/03/05 12:25:24 by wgulista         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-static int        check_line(char *str)
+static int			check_line(char *str)
 {
-    int i;
+	int		i;
 
-    i = 0;
-    if (!str)
-        return (-1);
-    while (str[i] && str[i] != '\n')
-        i++;
-    if (str[i] != '\n')
-        return (-1);
-    else
-        return (i);
+	i = 0;
+	if (!str)
+		return (-1);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] != '\n')
+		return (-1);
+	return (i);
 }
 
-static char        *ft_join(char **dest, char *src)
+static char			*ft_join(char **dest, char *src)
 {
-    char *temp;
+	char	*temp;
 
-    temp = NULL;
-    temp = ft_strjoin(*dest, src);
-    if (!temp)
-        return (NULL);
-    ft_strdel(dest);
-    return (temp);
+	temp = NULL;
+	temp = ft_strjoin(*dest, src);
+	if (!temp)
+		return (NULL);
+	ft_strdel(dest);
+	return (temp);
 }
 
-char            *ft_fill(int chk, char **rest, char **line)
+static char			*ft_fill(int chk, char **rest, char **line)
 {
-    char    *new_rest;
+	char	*new_rest;
 
-    new_rest = NULL;
-    if (!(*line = ft_strsub(*rest, 0, chk)))
-        return (NULL);
-    if (!(new_rest = ft_strsub(*rest, chk + 1, ft_strlen(*rest) - chk + 1)))
-        return (NULL);
-    ft_strdel(rest);
-    return (new_rest);
+	new_rest = NULL;
+	if (!(*line = ft_strsub(*rest, 0, chk)))
+		return (NULL);
+	if (!(new_rest = ft_strsub(*rest, chk + 1, ft_strlen(*rest) - chk + 1)))
+		return (NULL);
+	ft_strdel(rest);
+	return (new_rest);
 }
 
-int                get_next_line(int fd, char **line)
+int					get_next_line(int fd, char **line)
 {
-    static char    *rest[256];
-    char        buff[BUFF_SIZE + 1];
-    int            rd;
-    int            chk;
+	static char		*rest[256];
+	char			buff[BUFF_SIZE + 1];
+	int				rd;
+	int				chk;
 
-    if (!line || fd < 0)
-        return (-1);
-    rd = 0;
-    while ((chk = check_line(rest[fd])) == -1)
-    {
-        if ((rd = read (fd, buff, BUFF_SIZE)) == -1)
-            return (-1);
-        buff[rd] = '\0';
-        rest[fd] = ft_join(&rest[fd], buff);
-        if (!rest[fd])
-            return (-1);
-        ft_strclr(buff);
-        if (rd == 0)
-        {
-            *line = ft_join(&rest[fd], buff);
-            return (0);
-        }
-    }
-    rest[fd] = ft_fill(chk, &rest[fd], line);
-    return (1);
+	if (!line || fd < 0)
+		return (-1);
+	rd = 0;
+	while ((chk = check_line(rest[fd])) == -1)
+	{
+		if ((rd = read(fd, buff, BUFF_SIZE)) == -1)
+			return (-1);
+		buff[rd] = '\0';
+		rest[fd] = ft_join(&rest[fd], buff);
+		if (!rest[fd])
+			return (-1);
+		ft_strclr(buff);
+		if (rd == 0)
+		{
+			*line = ft_join(&rest[fd], buff);
+			return (0);
+		}
+	}
+	rest[fd] = ft_fill(chk, &rest[fd], line);
+	return (1);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,8 +1,19 @@
-#ifndef __GET_NEXT_LINE_H__
-# define __GET_NEXT_LINE_H__
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wgulista <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/05 12:25:30 by wgulista          #+#    #+#             */
+/*   Updated: 2016/03/05 12:25:31 by wgulista         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # include "libft/includes/libft.h"
 # include <fcntl.h>
-# include <stdio.h> 
 
 # define BUFF_SIZE 32
 
